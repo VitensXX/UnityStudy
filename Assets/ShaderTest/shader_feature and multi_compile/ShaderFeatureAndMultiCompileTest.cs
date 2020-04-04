c71ce14c996c7745824dfa2c1d94ad9c224a3743
Vitens <vitens@foxmail.com>
2020-04-04 21:05:48 +0800

    shader_feature and multi_compile

---
+++ Assets/ShaderTest/shader_feature and multi_compile/ShaderFeatureAndMultiCompileTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShaderFeatureAndMultiCompileTest : MonoBehaviour
+{
+    public Image shaderFeature;
+    public Image multiCompile;
+
+    bool _isRed = false;
+    Material _matShaderFeature;
+    Material _matMultiCompile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _matShaderFeature = shaderFeature.material;
+        _matMultiCompile = multiCompile.material;
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(100, 100, 100, 80), "Red"))
+        {
+            _matShaderFeature.EnableKeyword("_SHOW_RED_ON");
+            _matMultiCompile.EnableKeyword("_SHOW_RED_ON");
+        }
+        if (GUI.Button(new Rect(250, 100, 100, 80), "White"))
+        {
+            _matShaderFeature.DisableKeyword("_SHOW_RED_ON");
+            _matMultiCompile.DisableKeyword("_SHOW_RED_ON");
+        }
+    }
+
+}
---
+++ Assets/ShaderTest/shader_feature and multi_compile/multi_compile_test.shader
@@ -0,0 +1,53 @@
+﻿Shader "Test/multi_compile_test"
+{
+    Properties
+	{
+		[Toggle(_SHOW_RED_ON)] _DisplayColor ("显示红色?", float) = 0
+	}
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			//#pragma shader_feature _SHOW_RED_ON
+			#pragma multi_compile __ _SHOW_RED_ON
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				//原始显示为白色
+				fixed4 color = fixed4(1,1,1,1);
+
+				//勾选上显示红色
+				#if _SHOW_RED_ON
+					color = fixed4(1,0,0,1);
+				#endif
+
+				return color;
+			}
+			ENDCG
+		}
+	}
+
+	FallBack off
+}
