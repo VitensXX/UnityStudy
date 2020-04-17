083172d88e77ed5ad61c5e2517255f67802bbeeb
VitensXX <vitens@foxmail.com>
2020-04-17 19:34:31 +0800

    Bezier Curve

---
+++ Assets/Battle/ArmyView.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Battle
+{
+    public class ArmyView : MonoBehaviour
+    {
+        public enum AnimAction
+        {
+            Idle,
+            Attack01,
+            Attack02,
+            Attack03,
+            Hit,
+            Die
+        }
+
+        playerControl _animCtrl;
+        Material _mat;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //GO_missionDetails = transform.Find("Content/GO_missionDetails").gameObject;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SetAction(AnimAction action)
+        {
+            switch (action)
+            {
+                case AnimAction.Attack01:
+                    _animCtrl.Attack01();
+                    break;
+                case AnimAction.Attack02:
+                    _animCtrl.Attack02();
+                    break;
+                case AnimAction.Attack03:
+                    _animCtrl.Attack03();
+                    break;
+                case AnimAction.Idle:
+                    _animCtrl.Idle02();
+                    break;
+                case AnimAction.Die:
+                    _animCtrl.Die();
+                    break;
+                case AnimAction.Hit:
+                    _animCtrl.GetHit();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void HitFlash()
+        {
+            StartCoroutine(hitFlash());
+        }
+
+        IEnumerator hitFlash()
+        {
+            _mat.SetColor("_ColorGlitter", new Color(0.3f, 0.3f, 0.3f));
+            yield return new WaitForSeconds(0.1f);
+            _mat.SetColor("_ColorGlitter", Color.black);
+        }
+
+        Coroutine _dying;
+        public void Dying()
+        {
+            _dying = StartCoroutine(dying());
+        }
+
+        public void StopDying()
+        {
+            StopCoroutine(_dying);
+        }
+
+        IEnumerator dying()
+        {
+            while (true)
+            {
+                _mat.SetColor("_ColorGlitter", new Color(0.3f, 0, 0));
+                yield return new WaitForSeconds(0.2f);
+                _mat.SetColor("_ColorGlitter", Color.black);
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+    }
+}
---
+++ Assets/BezierCurve/Bezier.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bezier : MonoBehaviour
+{
+    public Slider slider;
+    public GameObject Ball;
+    public LineRenderer lineRenderer;
+    //public LineRenderer lineRenderer2;
+
+    Vector3 A = new Vector3(-10, 10, 0);
+    Vector3 B = new Vector3(0, 0, 0);
+    Vector3 C = new Vector3(10, 10, 0);
+    // Start is called before the first frame update
+
+    Dictionary<int, LineRenderer> _lineCache = new Dictionary<int, LineRenderer>();
+
+    List<Vector3> points = new List<Vector3>();
+    void Start()
+    {
+        points.Add(A);
+        points.Add(B);
+        points.Add(C);
+
+        lineRenderer.positionCount = points.Count;
+        for (int i = 0; i < points.Count; i++)
+        {
+            lineRenderer.SetPosition(i, points[i]);
+        }
+
+        //lineRenderer2.positionCount = 2;
+        _lineCache[points.Count] = lineRenderer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnValueChanged()
+    {
+        Ball.transform.position = BezierCurve(slider.value, points);
+    }
+
+    Vector3 LerpPoint(Vector3 A, Vector3 B, float lerp)
+    {
+        float x = Mathf.Lerp(A.x, B.x, lerp);
+        float y = Mathf.Lerp(A.y, B.y, lerp);
+        float z = Mathf.Lerp(A.z, B.z, lerp);
+
+        return new Vector3(x, y, z);
+    }
+
+    Vector3 BezierCurve(float lerp, List<Vector3> points)
+    {
+        if(points.Count == 1)
+        {
+            return points[0];
+        }
+
+        List<Vector3> p = new List<Vector3>();
+        for (int i = 0; i < points.Count - 1; i++)
+        {
+            p.Add(LerpPoint(points[i], points[i + 1], lerp));
+        }
+        //if(p.Count == 2)
+        //{
+
+        LineRenderer line;
+        _lineCache.TryGetValue(p.Count, out line);
+        if(line == null)
+        {
+            line = new GameObject("line", typeof(LineRenderer)).GetComponent<LineRenderer>();
+            _lineCache.Add(p.Count, line);
+        }
+
+        line.positionCount = p.Count;
+        for (int i = 0; i < p.Count; i++)
+        {
+            line.SetPosition(i, p[i]);
+        }
+
+        return BezierCurve(lerp, p);
+    }
+
+    void Recovery()
+    {
+
+    }
+}
