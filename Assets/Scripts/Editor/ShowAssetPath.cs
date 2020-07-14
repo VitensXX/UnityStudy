88025b7f0a15a90f541635e6f91a3d5f06def630
VitensXX <vitens@foxmail.com>
2020-07-14 19:16:53 +0800

    unity editor get select asset path

---
+++ Assets/Scripts/Editor/ShowAssetPath.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ShowAssetPath : Editor
+{
+    [MenuItem("Assets/LogPath")]
+    public static void LogPath()
+    {
+        //支持多选
+        string[] guids = Selection.assetGUIDs;
+        for (int i = 0; i < guids.Length; i++)
+        {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
+            Debug.Log(assetPath);
+        }
+    }
+
+    [MenuItem("Assets/LogPath", true)]
+    public static bool ValidateLogPath()
+    {
+        return false;//返回true则为正常可点击状态 false代表变灰不可点击状态
+    }
+}
