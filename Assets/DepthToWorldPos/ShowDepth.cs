c24d279f4b4683a6f8e11099d36f1050c795d552
VitensXX <vitens@foxmail.com>
2021-07-20 18:20:06 +0800

    depth

---
+++ Assets/DepthToWorldPos/ShowDepth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Created by Vitens on 2021/7/20 11:35:53
+/// 
+/// Description : 
+///     深度图测试
+/// </summary>
+public class ShowDepth : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
